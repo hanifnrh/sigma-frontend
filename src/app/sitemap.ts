a8a0cd96cc95e279ea13593228fc6b64d6af2093
hanirnrh <nurrahmathaniff@gmail.com>
2025-05-01 16:33:33 +0700

    Performance adjustment in login page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,54 +2,43 @@ import { MetadataRoute } from "next";
 
 export const baseUrl = "https://www.sigma-ta.com";
 
-async function fetchBlogSlugs() {
-    const response = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_URL!, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_TOKEN}`,
-        },
-        body: JSON.stringify({
-            query: `
-                query {
-                    blogs {
-                        slug
-                    }
-                    projects {
-                        slug
-                    }
-                }
-            `,
-        }),
-    });
-
-    const data = await response.json();
-    return {
-        blogSlugs: data?.data?.blogs || [],
-        projectSlugs: data?.data?.projects || [],
-    };
-}
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const { blogSlugs, projectSlugs } = await fetchBlogSlugs();
 
     const staticRoutes = [
         { url: `${baseUrl}/`, lastModified: new Date().toISOString() },
-        { url: `${baseUrl}/about`, lastModified: new Date().toISOString() },
-        { url: `${baseUrl}/guestbook`, lastModified: new Date().toISOString() },
-        { url: `${baseUrl}/blog`, lastModified: new Date().toISOString() },
-        { url: `${baseUrl}/projects`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tentang`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/bantuan`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/login`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/dashboard`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/data-ayam`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/grafik`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/informasi`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/perangkat-keras`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/profile`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/riwayat`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/standar-operasional`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/staf/umpan-balik`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/dashboard`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/data-ayam`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/grafik`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/informasi`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/perangkat-keras`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/profile`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/riwayat`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/standar-operasional`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/pemilik/umpan-balik`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/dashboard`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/data-ayam`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/grafik`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/informasi`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/perangkat-keras`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/profile`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/riwayat`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/standar-operasional`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/tamu/umpan-balik`, lastModified: new Date().toISOString() },
+        
     ];
 
-    const dynamicBlogRoutes = blogSlugs.map((post: { slug: string }) => ({
-        url: `${baseUrl}/blog/${post.slug}`,
-        lastModified: new Date().toISOString(),
-    }));
 
-    const dynamicProjectRoutes = projectSlugs.map((project: { slug: string }) => ({
-        url: `${baseUrl}/projects/${project.slug}`,
-        lastModified: new Date().toISOString(),
-    }));
-
-    return [...staticRoutes, ...dynamicBlogRoutes, ...dynamicProjectRoutes];
+    return [...staticRoutes];
 }
